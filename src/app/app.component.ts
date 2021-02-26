423b95f9f0277b4bce13673f67b289f84cc3194a
Darius Augsburger <darius.augsburger@redpoint.teseon.com>
2021-02-26 21:43:24 +0100

    added deployment as postbuild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,21 +11,21 @@ export class AppComponent {
   projects = [
     {
       name: 'PineappleMinds',
-      href: 'https://pineappleminds.com',
+      href: 'https://www.pineappleminds.com',
       link: 'pineappleminds.com',
       online: false,
       description: 'Website for students, collaborative work.'
     },
     {
       name: 'IT Base',
-      href: 'https://itbase.itea-factory.com',
-      link: 'itbase.itea-factory.com',
+      href: 'https://itbase.auxburger.com',
+      link: 'itbase.auxburger.com',
       online: false,
       description: 'Collection of abbreviations and descriptions of IT concepts.'
     },
     {
       name: 'OberSmart',
-      href: 'https://obersmart.de',
+      href: 'https://www.obersmart.de',
       link: 'obersmart.de',
       online: true,
       description: 'Application to order with qr codes on table.'
